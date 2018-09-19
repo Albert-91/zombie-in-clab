23d6a4e623e7a73372d9a92dbc5f58c49fa2ee4b
Albert-91 <albert.suralinski@gmail.com>
2018-09-19 21:03:49 +0200

    Moved collide_hit_rect

--- functions.py
+++ functions.py
@@ -5,21 +5,21 @@ def collide_hit_rect(one, two):
     return one.hit_rect.colliderect(two)
 
 
-def collide_with_object(self, direction, object_to_collide):
-    hits = pygame.sprite.spritecollide(self, object_to_collide, False, collide_hit_rect)
+def collide_with_object(sprite, group, direction):
+    hits = pygame.sprite.spritecollide(sprite, group, False, collide_hit_rect)
     if direction == 'x':
         if hits:
-            if self.vel.x > 0:
-                self.position.x = hits[0].rect.left - self.hit_rect.width / 2
-            if self.vel.x < 0:
-                self.position.x = hits[0].rect.right + self.hit_rect.width / 2
-            self.vel.x = 0
-            self.hit_rect.centerx = self.position.x
+            if sprite.vel.x > 0:
+                sprite.position.x = hits[0].rect.left - sprite.hit_rect.width / 2
+            if sprite.vel.x < 0:
+                sprite.position.x = hits[0].rect.right + sprite.hit_rect.width / 2
+            sprite.vel.x = 0
+            sprite.hit_rect.centerx = sprite.position.x
     if direction == 'y':
         if hits:
-            if self.vel.y > 0:
-                self.position.y = hits[0].rect.top - self.hit_rect.height / 2
-            if self.vel.y < 0:
-                self.position.y = hits[0].rect.bottom + self.hit_rect.height / 2
-            self.vel.y = 0
-            self.hit_rect.centery = self.position.y
+            if sprite.vel.y > 0:
+                sprite.position.y = hits[0].rect.top - sprite.hit_rect.height / 2
+            if sprite.vel.y < 0:
+                sprite.position.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
+            sprite.vel.y = 0
+            sprite.hit_rect.centery = sprite.position.y
