7d806a9825ee883cd552dfbaba35779b213932ed
Albert-91 <albert.suralinski@gmail.com>
2018-09-26 22:19:11 +0200

    Deleted Map class

--- screen.py
+++ screen.py
@@ -25,22 +25,6 @@ class Camera:
         self.screen = pygame.Rect(x, y, self.width, self.height)
 
 
-class Map:
-
-    def __init__(self, filename):
-        self.map_data = []
-        with open(filename, 'rt') as lines:
-            for line in lines:
-                self.map_data.append(line)
-        self.tiles_quantity_row = len(self.map_data[0]) - 1
-        self.tiles_quantity_col = len(self.map_data)
-        self.width = self.tiles_quantity_row * WALL_SIZE
-        self.height = self.tiles_quantity_col * WALL_SIZE
-
-    def get_map(self):
-        return self.map_data
-
-
 class TiledMap:
 
     def __init__(self, filename):
