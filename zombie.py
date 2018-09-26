e9d1f95c6eff822e26ff82a14adc31396a37fbf4
Albert-91 <albert.suralinski@gmail.com>
2018-09-26 19:46:49 +0200

    Zombies has less probability to be on each other

--- zombie.py
+++ zombie.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 from functions import collide_with_object
 from settings import *
 
@@ -20,6 +20,7 @@ class Zombie(pygame.sprite.Sprite):
         self.rotation = 0
         self.shield = ZOMBIE_SHIELD
         self.shield_bar = None
+        self.speed = choice(ZOMBIE_SPEEDS_EASY)
 
     def natural_moves(self):
         moves_list = [0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 0]
@@ -27,15 +28,14 @@ class Zombie(pygame.sprite.Sprite):
         y = randint(0, len(moves_list) - 1)
         self.move(dx=moves_list[x], dy=moves_list[y])
 
-    def attack(self, attack, victim):
-        victim.shield -= attack
-
     def update(self):
         self.rotation = (self.game.player.position - self.position).angle_to(vector(1, 0))
         self.image = pygame.transform.rotate(self.game.zombie_img, self.rotation)
         self.rect = self.image.get_rect()
         self.rect.center = self.position
-        self.acc = vector(ZOMBIE_SPEED_EASY, 0).rotate(-self.rotation)
+        self.acc = vector(1, 0).rotate(-self.rotation)
+        self.avoid_other_zombies()
+        self.acc.scale_to_length(self.speed)
         self.acc += self.vel * (-1)
         self.vel += self.acc * self.game.dt
         self.position += self.vel * self.game.dt + (self.acc * self.game.dt ** 2) / 2
@@ -58,3 +58,11 @@ class Zombie(pygame.sprite.Sprite):
         self.shield_bar = pygame.Rect(0, 0, width, 7)
         if self.shield < ZOMBIE_SHIELD:
             pygame.draw.rect(self.image, color, self.shield_bar)
+
+    def avoid_other_zombies(self):
+        for zombie in self.game.zombies:
+            if zombie != self:
+                distance = self.position - zombie.position
+                if 0 < distance.length() < AVOID_RADIUS:
+                    self.acc += distance.normalize()
+
