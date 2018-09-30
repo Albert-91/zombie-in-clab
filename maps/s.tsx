2bf17d300ff654bc15d203a2bda44d93536d55ab
Albert-91 <albert.suralinski@gmail.com>
2018-09-30 16:32:03 +0200

    Added furniture object to kitchen in map.

---
+++ maps/s.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="s" tilewidth="87" tileheight="87" tilecount="1" columns="1">
+ <image source="../images/smokeGrey0.png" width="87" height="87"/>
+</tileset>
---
+++ maps/y.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="y" tilewidth="87" tileheight="87" tilecount="1" columns="1">
+ <image source="../images/smokeYellow0.png" width="87" height="87"/>
+</tileset>
