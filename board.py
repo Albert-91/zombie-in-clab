99dce4c36cc2e22b7e47b1939b48a2dbac46019c
Albert-91 <albert.suralinski@gmail.com>
2018-10-04 21:10:12 +0200

    Changed screen size to that values from settings parameters, and uncommented drawing loops.

--- board.py
+++ board.py
@@ -4,7 +4,7 @@ from settings import *
 class Board:
 
     def __init__(self, width, height):
-        self.surface = pg.display.set_mode((width, height), pg.FULLSCREEN)
+        self.surface = pg.display.set_mode((width, height), 0, 32)
         pg.display.set_caption('Zombie in CLab')
         self.width = width
         self.height = height
@@ -17,15 +17,15 @@ class Board:
         self.difficulty_font = pg.font.Font(my_font, 70)
         self.game_over_font = pg.font.Font(my_font, 150)
 
-    def draw_menu(self, object):
+    def draw_menu(self, *args):
         self.intro_bg = pg.transform.scale(self.intro_bg, (self.width, self.height))
         self.surface.blit(self.intro_bg, (0, 0), (0, 0, self.width, self.height))
-        self.draw_text(self.surface, "Zombie in CLab", self.width / 2, self.height * 0.3,
-                       self.title_font)
+        self.draw_text(self.surface, "Zombie in CLab", self.width / 2, self.height * 0.3, self.title_font)
         self.draw_text(self.surface, "Play", self.width / 2, self.height * 0.6, self.menu_font)
         self.draw_text(self.surface, "Options", self.width / 2, self.height * 0.7, self.menu_font)
         self.draw_text(self.surface, "Quit", self.width / 2, self.height * 0.8, self.menu_font)
-        object.draw_on(object.surface)
+        for drawable in args:
+            drawable.draw_on(self.surface)
         pg.display.update()
 
     def draw_options(self, *args):
@@ -34,9 +34,8 @@ class Board:
         self.draw_text(self.surface, "Controls", self.width / 2, self.height * 0.35, self.options_font)
         self.draw_text(self.surface, "Audio", self.width / 2, self.height * 0.5, self.options_font)
         self.draw_text(self.surface, "Return", self.width / 2, self.height * 0.65, self.options_font)
-        # for drawable in args:
-        #     drawable.draw_on(self.surface)
-
+        for drawable in args:
+            drawable.draw_on(self.surface)
         pg.display.update()
 
     def draw_game_over(self, name, *args):
@@ -44,9 +43,8 @@ class Board:
         self.surface.fill(background)
         self.draw_text(self.surface, "Game over", self.width / 2, self.height * 0.4,
                        self.game_over_font)
-        # for drawable in args:
-        #     drawable.draw_on(self.surface)
-
+        for drawable in args:
+            drawable.draw_on(self.surface)
         pg.display.update()
 
     def draw_choosing_difficulty(self, *args):
@@ -56,8 +54,8 @@ class Board:
         self.draw_text(self.surface, "Normal", self.width / 2, self.height * 0.4, self.difficulty_font)
         self.draw_text(self.surface, "Hard", self.width / 2, self.height * 0.6, self.difficulty_font)
         self.draw_text(self.surface, "Zombie hell!", self.width / 2, self.height * 0.8, self.difficulty_font)
-        # for drawable in args:
-        #     drawable.draw_on(self.surface)
+        for drawable in args:
+            drawable.draw_on(self.surface)
 
         pg.display.update()
 
