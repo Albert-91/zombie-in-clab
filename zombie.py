a351d93677574d16d46b8498efce8556345119ab
Albert <albert@greenparrot.pl>
2019-08-17 23:45:52 +0200

    [Refactoring] Refactored Zombie class.

--- zombie.py
+++ zombie.py
@@ -29,38 +29,6 @@ class Zombie(pg.sprite.Sprite):
         self.damaged = False
         self.damage_alpha = None
 
-    def update(self):
-        target_distance = self.target.position - self.position
-        if target_distance.length_squared() < DETECT_RADIUS ** 2:
-            if random() < 0.002:
-                choice(self.game.zombie_moan_sounds).play()
-            self.rotation = (self.game.player.position - self.position).angle_to(vector(1, 0))
-            self.image = pg.transform.rotate(self.game.zombie_img, self.rotation)
-            if self.damaged:
-                try:
-                    self.image.fill((255, 0, 0, next(self.damage_alpha)), special_flags=pg.BLEND_RGB_MULT)
-                except StopIteration:
-                    self.damaged = False
-            self.rect = self.image.get_rect()
-            self.rect.center = self.position
-            self.acc = vector(1, 0).rotate(-self.rotation)
-            self.avoid_other_zombies()
-            self.acc.scale_to_length(self.speed)
-            self.acc += self.vel * (-1)
-            self.vel += self.acc * self.game.dt
-            self.position += self.vel * self.game.dt + (self.acc * self.game.dt ** 2) / 2
-            self.hit_rect.centerx = self.position.x
-            collide_with_object(self, self.game.walls, 'x')
-            self.hit_rect.centery = self.position.y
-            collide_with_object(self, self.game.walls, 'y')
-            self.rect.center = self.hit_rect.center
-        if self.shield <= 0:
-            size = randint(70, 120)
-            Smoke(self.game, self.rect.center, self.game.zombie_death_smoke, size)
-            choice(self.game.zombie_die_sounds).play()
-            self.kill()
-            self.game.map_img.blit(choice(self.game.splats), self.position - vector(32, 32))
-
     def draw_shield(self):
         if self.shield > 60:
             color = GREEN
@@ -73,10 +41,59 @@ class Zombie(pg.sprite.Sprite):
         if self.shield < ZOMBIE_SHIELD:
             pg.draw.rect(self.image, color, self.shield_bar)
 
-    def avoid_other_zombies(self):
+    def update(self):
+        target_distance = self.target.position - self.position
+        if target_distance.length_squared() < DETECT_RADIUS ** 2:
+            self._update_zombie_moan_sounds()
+            self._update_image()
+            self._update_damage()
+            self._update_position()
+            self._update_collisisions()
+        if self.shield <= 0:
+            self.die()
+
+    def die(self):
+        size = randint(70, 120)
+        Smoke(self.game, self.rect.center, self.game.zombie_death_smoke, size)
+        choice(self.game.zombie_die_sounds).play()
+        self.kill()
+        self.game.map_img.blit(choice(self.game.splats), self.position - vector(32, 32))
+
+    def _update_collisisions(self):
+        self._avoid_other_zombies()
+        collide_with_object(self, self.game.walls, 'x')
+        collide_with_object(self, self.game.walls, 'y')
+
+    def _avoid_other_zombies(self):
         for zombie in self.game.zombies:
             if zombie != self:
                 distance = self.position - zombie.position
                 if 0 < distance.length() < AVOID_RADIUS:
                     self.acc += distance.normalize()
 
+    def _update_zombie_moan_sounds(self):
+        if random() < 0.002:
+            choice(self.game.zombie_moan_sounds).play()
+
+    def _update_damage(self):
+        if self.damaged:
+            try:
+                self.image.fill((255, 0, 0, next(self.damage_alpha)), special_flags=pg.BLEND_RGB_MULT)
+            except StopIteration:
+                self.damaged = False
+
+    def _update_image(self):
+        self.rotation = (self.game.player.position - self.position).angle_to(vector(1, 0))
+        self.image = pg.transform.rotate(self.game.zombie_img, self.rotation)
+
+    def _update_position(self):
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
+        self.acc = vector(1, 0).rotate(-self.rotation)
+        self.acc.scale_to_length(self.speed)
+        self.acc += self.vel * (-1)
+        self.vel += self.acc * self.game.dt
+        self.position += self.vel * self.game.dt + (self.acc * self.game.dt ** 2) / 2
+        self.hit_rect.centerx = self.position.x
+        self.hit_rect.centery = self.position.y
+        self.rect.center = self.hit_rect.center
